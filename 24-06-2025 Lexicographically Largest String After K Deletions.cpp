40a959400afffa23e719582c5c3e3247eda158a0
RohitGit110 <rohitkumaryy342@gmail.com>
2025-06-25 01:35:37 +0530

    Create 24-06-2025 Lexicographically Largest String After K Deletions.cpp

---
+++ 24-06-2025 Lexicographically Largest String After K Deletions.cpp
@@ -0,0 +1,25 @@
+// Problem Name  Lexicographically Largest String After K Deletions
+// Topic      Array + String 
+
+class Solution {
+  public:
+    string maxSubseq(string& s, int k) {
+        // code here
+        string ans;
+        for(int i=0;i<s.size();i++)
+        {
+            while(ans.size()>0 && ans.back()<s[i] && k>0)
+            {
+                ans.pop_back();
+                k--;
+            }
+            ans.push_back(s[i]);
+        }
+        while(k>0)
+        {
+            ans.pop_back();
+            k--;
+        }
+        return ans;
+    }
+};
